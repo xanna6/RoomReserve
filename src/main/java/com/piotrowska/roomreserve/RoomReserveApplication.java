d933cf5c6b99251742df17afe58549a25d35ec4f
xanna6 <apiotrow96@gmail.com>
2023-06-22 19:53:44 +0200

    utworzenie projektu

---
+++ src/main/java/com/piotrowska/roomreserve/RoomReserveApplication.java
@@ -0,0 +1,13 @@
+package com.piotrowska.roomreserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomReserveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomReserveApplication.class, args);
+    }
+
+}

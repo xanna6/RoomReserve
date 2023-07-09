f9824fa0dc57ba4c8eb48c158996654dd0a4f13e
xanna6 <apiotrow96@gmail.com>
2023-07-09 22:13:44 +0200

    creation of guest entity

---
+++ src/main/java/com/piotrowska/roomreserve/entity/Guest.java
@@ -0,0 +1,67 @@
+package com.piotrowska.roomreserve.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Guest {
+
+    @Id
+    private Long id;
+    private String name;
+    private String surname;
+    private String phoneNumber;
+    private String mail;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    @Override
+    public String toString() {
+        return "Guest{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", mail='" + mail + '\'' +
+                '}';
+    }
+}

96b4f64644a9a510d5b433494f0038085171cc56
xanna6 <apiotrow96@gmail.com>
2023-07-18 18:47:06 +0200

    adding ReservationRepository

--- src/main/java/com/piotrowska/roomreserve/controller/ReservationController.java
+++ src/main/java/com/piotrowska/roomreserve/controller/ReservationController.java
@@ -1,13 +1,28 @@
 package com.piotrowska.roomreserve.controller;
 
+import com.piotrowska.roomreserve.entity.RoomGuest;
+import com.piotrowska.roomreserve.repository.ReservationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class ReservationController {
+    private final ReservationRepository reservationRepository;
+
+    @Autowired
+    public ReservationController(ReservationRepository reservationRepository) {
+        this.reservationRepository = reservationRepository;
+    }
 
     @GetMapping("/reservations")
     public String getReservations() {
+        List<RoomGuest> reservations = this.reservationRepository.findAllReservations();
+        for (RoomGuest rg: reservations) {
+            System.out.println(rg.toString());
+        }
         return "reservations";
     }
 }
---
+++ src/main/java/com/piotrowska/roomreserve/repository/ReservationRepository.java
@@ -0,0 +1,13 @@
+package com.piotrowska.roomreserve.repository;
+
+import com.piotrowska.roomreserve.entity.RoomGuest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface ReservationRepository extends JpaRepository<RoomGuest, Long> {
+
+    @Query("SELECT rg FROM RoomGuest rg JOIN Guest g ON rg.guest.id=g.id ORDER BY rg.creationDate desc")
+    List<RoomGuest> findAllReservations();
+}
